dacb111372059ea06f6ae5a2724a377e7ee6201f
ayachi amina <uvsq21807869@bitbucket.org>
2020-03-04 02:38:30 +0100

    ajout de la methode de calcul de la salaire  auquel s’ajoute 20€ par année d’ancienneté

--- pgpl_2/src/main/java/uvsq/fr/pgpl_2/Employe.java
+++ pgpl_2/src/main/java/uvsq/fr/pgpl_2/Employe.java
@@ -1,5 +1,7 @@
 package uvsq.fr.pgpl_2;
 
+import java.util.Calendar;
+
 public class Employe {
 	private final String nomEmp;
 	private final int annee_arr;
@@ -17,5 +19,10 @@ public class Employe {
 
 	public int getAnnee_arr() {
 		return annee_arr;
+	}
+	public double calculSalaire() {
+		Calendar c = Calendar.getInstance();
+		int annee_courante = c.get(Calendar.YEAR);
+		return 1500+20*(annee_courante-annee_arr);
 	}
 }
